cc46aa7487b84c66a7d4151055f757b7fe9b840b
yb9255 <yoobin9255@gmail.com>
2023-02-06 17:36:17 +0900

    <feature> add body types and validtion with dto on users/createUser

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { Body, Controller, Post } from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    console.log(body);
+  }
+}
