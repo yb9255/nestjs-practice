640a79a8129a1e331c93e1995a54f81dedebbfd8
yb9255 <yoobin9255@gmail.com>
2023-02-06 18:02:07 +0900

    <feature> create first api with users service/repository

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,13 @@
+import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { Body, Controller, Post } from '@nestjs/common';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    console.log(body);
+    this.usersService.create(body.email, body.password);
   }
 }
