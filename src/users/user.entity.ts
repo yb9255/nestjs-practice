8c19b73f3788e940d9b5ad9d79481d165a01cf87
yb9255 <yoobin9255@gmail.com>
2023-02-06 19:40:30 +0900

    <feature> add hook to user entity for practice

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
@@ -10,4 +17,19 @@ export class User {
 
   @Column()
   password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted User with id', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated with User Id', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed with User Id', this.id);
+  }
 }
