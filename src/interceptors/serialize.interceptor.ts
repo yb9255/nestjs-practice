b38fa9a513a619b7065b32035a068d036c92be5a
yb9255 <yoobin9255@gmail.com>
2023-02-11 02:49:51 +0900

    <feature> use class-transformer ClassConstructor

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -6,29 +6,25 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { plainToInstance } from 'class-transformer';
+import { ClassConstructor, plainToInstance } from 'class-transformer';
 //강의에서는 plainToClass를 사용하고 있으나 deprecated됨
 
-interface ClassConstructor {
-  new (...args: any[]): {};
-}
-
-export function Serialize(dto: ClassConstructor) {
+export function Serialize<T>(dto: ClassConstructor<T>) {
   return UseInterceptors(new SerializeInterceptor(dto));
 }
 
-export class SerializeInterceptor implements NestInterceptor {
-  constructor(private dto: any) {}
+export class SerializeInterceptor<T> implements NestInterceptor {
+  constructor(private dto: ClassConstructor<T>) {}
 
   intercept(
     context: ExecutionContext,
-    next: CallHandler<any>, // 강의에서는 handler라는 인자 이름 사용
-  ): Observable<any> | Promise<Observable<any>> {
+    next: CallHandler<T>, // 강의에서는 handler라는 인자 이름 사용
+  ): Observable<T> | Promise<Observable<T>> {
     // Run something before a request is handled
     // by the request handler
 
     return next.handle().pipe(
-      map((data: any) => {
+      map((data: T) => {
         // Run Something before the response is sent out
         return plainToInstance(this.dto, data, {
           excludeExtraneousValues: true,
