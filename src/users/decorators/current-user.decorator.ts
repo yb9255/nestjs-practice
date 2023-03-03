3d0593327dfe9ab47a62b84b728161b79900b619
yb9255 <yoobin9255@gmail.com>
2023-03-03 23:58:31 +0900

    <chore> fix param name

--- src/users/decorators/current-user.decorator.ts
+++ src/users/decorators/current-user.decorator.ts
@@ -1,7 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const CurrentUser = createParamDecorator(
-  (data: never, context: ExecutionContext) => {
+  (_: never, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
     return request.currentUser;
   },
