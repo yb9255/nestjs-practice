11a4ee0608d97d81d9d4e90eca9814850fc5ed3b
yb9255 <yoobin9255@gmail.com>
2023-03-08 10:05:48 +0900

    <feature> add type on Express namespace

--- src/users/middlewares/current-user.middleware.ts
+++ src/users/middlewares/current-user.middleware.ts
@@ -1,7 +1,15 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { UsersService } from '../users.service';
+import { User } from '../user.entity';
 
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: User;
+    }
+  }
+}
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
   constructor(private usersService: UsersService) {}
@@ -11,7 +19,6 @@ export class CurrentUserMiddleware implements NestMiddleware {
 
     if (userId) {
       const user = await this.usersService.findOneBy(userId);
-      // @ts-ignore
       req.currentUser = user;
     }
 
